8c482c21f41b81d51f409d40a54a676264320d36
Mohamed Ahmed <programmer2011515616@gmail.com>
2021-09-08 01:02:02 +0200

    ending frontend

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express');
-    const path = require('path');
+const express = require("express");
+const path = require("path");
 
-    const app = express();
+const app = express();
 
-    // Serve only the static files form the dist directory
-    app.use(express.static(__dirname + '/dist/developers-articles'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + "/dist/developers-articles"));
 
-    app.get('/*', function(req,res) {
-        
-    res.sendFile(path.join(__dirname+'/dist/developers-articles/index.html'));
-    });
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/developers-articles/index.html"));
+});
 
-    // Start the app by listening on the default Heroku port
-    app.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8800);
